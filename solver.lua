a7aad3e90604e5ab5c9c4da85143221a47fa7e59
DVN <hammadiisa3004@gmail.com>
2026-02-17 22:40:49 +0700

    loadstring

--- solver.lua
+++ solver.lua
@@ -1,18 +1,6 @@
--- Konfigurasi
--- Masukkan API Key WinterCode Anda di sini
 WinterConfig = {
     ApiKey = "ISI_API_KEY_DISINI",
     PlaceId = 4483381587
 }
 
-print("❄️ Connecting...")
-local url = "https://raw.githubusercontent.com/dvn-hub/solver/main/source.lua"
-local handle = io.popen("curl -s -k " .. url)
-local script = handle:read("*a")
-handle:close()
-
-if script and #script > 10 then
-    load(script)()
-else
-    print("❌ Gagal download script!")
-end
+loadstring(io.popen("curl -s -k https://raw.githubusercontent.com/dvn-hub/solver/main/source.lua"):read("*a"))()
